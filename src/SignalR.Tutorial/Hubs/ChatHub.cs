ff48f0a8b06eff2cce43b15b9216b867d8ad9859
Jason V. Mock <jason.mock@lrs.com>
2018-04-18 17:43:21 -0600

    Initial commit.

---
+++ src/SignalR.Tutorial/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Tutorial.Hubs
+{
+    [Authorize]
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "left");
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("SendMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
---
+++ src/SignalR.Tutorial/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalR.Tutorial.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
